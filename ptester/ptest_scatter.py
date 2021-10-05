a47a76e7844f0282540a4c61097c63a5d4fe2b9c
Jaewook Cho <jae7cho@gmail.com>
2021-10-05 15:54:01 -0400

    default options

--- ptester/ptest_scatter.py
+++ ptester/ptest_scatter.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
 
-def ptest_scatter(x,y,figsize,marker,s):
+def ptest_scatter(x,y,figsize=(10,10),marker='.',s=10):
     plt.figure(figsize=(figsize[0],figsize[1]))
     plt.scatter(x,y,marker,s)
