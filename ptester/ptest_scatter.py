b94cbacd47a1748379a3cba079c50da41622b88b
Jaewook Cho <jae7cho@gmail.com>
2021-10-05 15:59:26 -0400

    Update ptest_scatter

--- ptester/ptest_scatter.py
+++ ptester/ptest_scatter.py
@@ -2,4 +2,4 @@ import matplotlib.pyplot as plt
 
 def ptest_scatter(x,y,figsize=(10,10),marker='.',s=10):
     plt.figure(figsize=(figsize[0],figsize[1]))
-    plt.scatter(x,y,marker,s)
+    plt.scatter(x,y,marker=marker,s=s)
