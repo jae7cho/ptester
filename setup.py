24e0a5350e683e0e516d3302f15a7c93d745b590
Jaewook Cho <jae7cho@gmail.com>
2021-10-08 12:19:38 -0400

    setup.py set up

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ptester',
+      version='1.0',
+      description='Test packaging',
+      author='Jae Wook Cho',
+      author_email='jae7cho@gmail.com',
+      url='https://github.com/jae7cho/ptester',
+      packages=['numpy', 'matplotlib'],
+     )
